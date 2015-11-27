5ecca61f640f893ad1964e29092248f019c405cb
Nacho <daw2ilopez@iesjoanramisorg>
2015-11-27 20:22:05 +0100

    Test Event

--- index.php
+++ index.php
@@ -13,22 +13,12 @@
 <div class="container">
     <div class="row">
         <div class="col s12 m4">
-            <div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src='img/sant-joan.jpg'>
-                </div>
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">Card Title<i
-                            class="material-icons right">more_vert</i></span>
+            <?php
+            include 'classes/Event.php';
 
-                    <p><a href="#">This is a link</a></p>
-                </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-
-                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                </div>
-            </div>
+            $event = new Event("4", "deportes", "pepito", "vives llull 15", "mahon", "Lorem ipsum", "28-11-2015", "29-11-2015", "sant-joan.jpg", 14);
+            $event->paintEvent();
+            ?>
         </div>
         <div class="col s12 m4">
             <div class="card">

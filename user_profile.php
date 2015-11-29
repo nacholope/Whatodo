28598287481288e9a52536070c01a38383a98103
dperches <daw2dperches@iesjoanramis.org>
2015-11-29 10:49:39 +0100

    User profile form added.

--- user_profile.php
+++ user_profile.php
@@ -17,16 +17,51 @@
 <main>
     <div class="container">
         <div class="row">
-            <ul class="collapsible" data-collapsible="accordion">
+            <ul class="collapsible popout" data-collapsible="accordion">
                 <li>
-                    <div class="collapsible-header"><i class="material-icons">filter_drama</i>Mis datos</div>
+                    <div class="collapsible-header"><i class="material-icons">person_pin</i>Mis datos</div>
                     <div class="collapsible-body">
-                        <p>Lorem ipsum dolor sit amet.</p>
+                        <form class="custom-padding">
+                            <div class="row">
+                                <div class="input-field col s6">
+                                    <input id="name" type="text" class="validate" required>
+                                    <label for="name" data-error="Incorrecto" data-success="Correcto">Nombre</label>
+                                </div>
+                                <div class="input-field col s6">
+                                    <input id="surname" type="text" class="validate" required>
+                                    <label for="surname" data-error="Incorrecto" data-success="Correcto">Apellidos</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field col s6">
+                                    <input id="email" type="email" class="validate" required>
+                                    <label for="email" data-error="Incorrecto" data-success="Correcto">Email</label>
+                                </div>
+                                <div class="input-field col s6">
+                                    <input id="password" type="password" class="validate" required>
+                                    <label for="password">Contrasenya</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col s12">
+                                    <button class="btn waves-effect waves-light" type="submit" name="action">
+                                        Actualizar
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </li>
                 <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Darse de baja</div>
-                    <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                    <div class="collapsible-header"><i class="material-icons">error</i>Darse de baja</div>
+                    <div class="collapsible-body">
+                        <p>Al darse de baja, no podr&aacute; volver a acceder a sus datos.</p>
+                        <div class="custom-padding">
+                            <button class="btn waves-effect waves-light" type="submit" name="action">
+                                Confirmar
+                            </button>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </div>

56370c98a9c030ace3b0a0acec6b2c08178c07a2
Nacho <daw2ilopez@iesjoanramisorg>
2015-11-29 12:29:45 +0100

    Terms validation JS

--- signup.php
+++ signup.php
@@ -70,15 +70,15 @@
                 <div class="row">
                     <div class="col s12">
                         <p>
-                            <input type="checkbox" id="rememberMe" name="conditions"/>
-                            <label for="rememberMe">He le&iacute;do y acepto los <a href="#">t&eacute;rminos y
+                            <input type="checkbox" id="terms"/>
+                            <label for="terms">He le&iacute;do y acepto los <a href="#">t&eacute;rminos y
                                     condiciones</a>.</label>
                         </p>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col s12">
-                        <button class="btn waves-effect waves-light" type="submit" name="action">
+                            <button class="btn waves-effect waves-light" disabled="disabled" type="submit" id="submit">
                             Registrarse
                         </button>
                     </div>
@@ -97,7 +97,13 @@ echo Script::JQUERY;
 echo Script::MATERIALIZE;
 ?>
 <script type="application/javascript">
-    //todo validate checkbox
+    $('#terms').click(function(){
+        if($('#submit').prop("disabled") == true){
+            $('#submit').prop("disabled",false);
+        }else{
+            $('#submit').prop("disabled",true);
+        }
+    });
 </script>
 </body>
 </html>

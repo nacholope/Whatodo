7eb7388a7684f5f27cd4c6fe0e33ef3e73932f9d
nacholope <daw2ilopez@iesjoanramis.org>
2015-11-30 19:15:13 +0100

    Footer Year Fixed

--- components/Footer.php
+++ components/Footer.php
@@ -11,7 +11,7 @@
     </div>
     <div class="footer-copyright">
         <div class="container">
-            © 2014 Copyright Text
+            © 2015 Copyright Text
             <a class="grey-text text-lighten-4 right" href="#!">Links</a>
         </div>
     </div>
